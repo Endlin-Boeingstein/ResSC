ff1d470300d226ffd1465fb9f1b0648814c915c5
Endlin Boeingstein <93501017+Endlin-Boeingstein@users.noreply.github.com>
2021-11-01 15:12:38 +0800

    Update README.md

--- Program.cs
+++ Program.cs
@@ -17,17 +17,17 @@ class Program
 		if (args.Length > 0)
 		{
 			FileInfo fileInfo = new FileInfo(args[0]);
-			Console.WriteLine("版本号：1.0.0-public-alpha\t作者：Endlin Boeingstein（滨敔滨纵凝）\n编译时间：2021年9月10日20时05分\t协助调试：某Eight-Bit\n在使用本软件前请提前备份好文件，否则后果自负");
+			Console.WriteLine("版本号：1.0.1-public-alpha\t作者：Endlin Boeingstein（滨敔滨纵凝）\n编译时间：2021年9月10日20时05分\t协助调试：某Eight-Bit\n在使用本软件前请提前备份好文件，否则后果自负");
 			Console.WriteLine("文件路径：" + fileInfo.FullName);
 			filepath = fileInfo.FullName;
 		}
 		//打开程序拖入窗体读取
 		else
 		{
-			Console.WriteLine("版本号：1.0.0-public-alpha\t作者：Endlin Boeingstein（滨敔滨纵凝）\n编译时间：2021年9月10日20时05分\t协助调试：某Eight-Bit\n在使用本软件前请提前备份好文件，否则后果自负");
+			Console.WriteLine("版本号：1.0.1-public-alpha\t作者：Endlin Boeingstein（滨敔滨纵凝）\n编译时间：2021年9月10日20时05分\t协助调试：某Eight-Bit\n在使用本软件前请提前备份好文件，否则后果自负");
 			Console.WriteLine("请将文件拖入窗体，并按回车键");
-			Console.WriteLine("如果你的文件在C盘（尤其是桌面），那么请退出程序，直接将文件拖放到本应用的图标即可，拖窗体模式无权限修改C盘文件");
-			filepath = Console.ReadLine();
+			//新功能更新而停用///Console.WriteLine("如果你的文件在C盘（尤其是桌面），那么请退出程序，直接将文件拖放到本应用的图标即可，拖窗体模式无权限修改C盘文件");
+			filepath = Console.ReadLine().Trim('"');
 		}
 		//创建jjof实例
 		JudgeJoF jjof = new JudgeJoF();
